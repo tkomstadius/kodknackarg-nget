65347e1bf5e877a83f7fc706e9210fa7c7ac1b0e
alundin <alilu684@student.liu.se>
2015-03-14 17:37:54 +0100

    merging alices and kajsas work to search for jobs in lan and yrke and get result

--- javascript/databaseHandler.js
+++ javascript/databaseHandler.js
@@ -1,10 +1,12 @@
 if (window.XMLHttpRequest)
 	  {// code for IE7+, Firefox, Chrome, Opera, Safari
 	  xmlhttp=new XMLHttpRequest();
+	  xmlhttp2=new XMLHttpRequest();
 	  }
 	else
 	  {// code for IE6, IE5
 	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  xmlhttp2=new ActiveXObject("Microsoft.XMLHTTP");
 	  }
 
 // var url = "http://api.arbetsformedlingen.se/platsannons/soklista/kommuner?lanid=10.xml";
@@ -14,14 +16,24 @@ xmlDoc=xmlhttp.responseXML;
 
 // Search function for län.
 var x=xmlDoc.getElementsByTagName("sokdata");
-var number = 1;
-
+var countLan = 1;
 
 for (i=0;i<x.length;i++)
   { 
-  	// console.log(number);
-  	document.getElementById(number).innerHTML = x[i].getElementsByTagName("namn")[0].childNodes[0].nodeValue;
-  	number++;
+  	// console.log(countLan);
+  	document.getElementById(countLan).innerHTML = x[i].getElementsByTagName("namn")[0].childNodes[0].nodeValue;
+  	countLan++;
   }
+xmlhttp2.open("GET","database/yrkesomraden.xml", false);
+xmlhttp2.send();
+xmlDoc2 = xmlhttp2.responseXML;
 
-
+// Search function for yrkesomraden
+var y = xmlDoc2.getElementsByTagName("sokdata");
+var countYrke = 23; 
+for (i=0;i<y.length;i++)
+  { 
+  	// console.log(countYrke);
+  	document.getElementById(countYrke).innerHTML = y[i].getElementsByTagName("namn")[0].childNodes[0].nodeValue;
+  	countYrke++;
+  }
