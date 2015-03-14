4b535b91f7c9d8741cc0c3d4279393aac3f7c315
alundin <alilu684@student.liu.se>
2015-03-15 00:25:19 +0100

    Finished search-job page

--- javascript/getData.js
+++ javascript/getData.js
@@ -11,16 +11,12 @@ function getAds(){
 	// Arbetsförmedlingen har en spärr på sin databas från http-request. De ska eventuellt ta bort denna i framtiden (eftersom vi lyfte frågan) 
 	// men kan inte göra något just nu. Vi får därför "fulkoda" från vårt egna provisoriska databassystem. 	
 	//  xmlhttp.open("GET","http://api.arbetsformedlingen.se/platsannons/soklista/kommuner?lanid=10",false);
-	xmlhttp.open("GET","database/data_it_stockholm.xml",false);
+	xmlhttp.open("GET","../database/data_it_stockholm.xml",false);
 	xmlhttp.send();
 	xmlDoc=xmlhttp.responseXML; 
 
 	var toDisplay; 
-	
-	if(document.getElementById("searchResult").innerHTML == ""){
-		document.getElementById("button1").innerHTML = "Take me back!";
-		console.log("den e noll");
-		var x=xmlDoc.getElementsByTagName("matchningdata");
+	var x=xmlDoc.getElementsByTagName("matchningdata");
 		toDisplay = "<br><br><h3>Jobb som passar dina kriterier</h3><br><br>";
 		//console.log(x.length);
 		//for (i=0;i<x.length;i++)
@@ -34,13 +30,7 @@ function getAds(){
 		  	}
 
 		document.getElementById("searchResult").innerHTML = toDisplay;
-	}
-	else {
-		document.getElementById("button1").innerHTML = "Show me some jobs!";
-		document.getElementById("searchResult").innerHTML = "";
-		console.log("den e nånting");
-	}
-	
+			
 }
 function matchAd(){
 	xmlhttp.open("GET","database/jobb2_data_it_stockholm_chef.xml",false);
@@ -49,4 +39,8 @@ function matchAd(){
 	var x = xmlDoc.getElementsByTagName("annons"); 
 	console.log(x[0]);
 	alert(x[0].getElementsByTagName("annonstext")[0].childNodes[0].nodeValue);
+}
+function getButton(){
+	var display = "<a href='#process'><div id='get-job' onclick='getAds()''></div></a>";							
+	document.getElementById("showButton").innerHTML = display;
 }
